e1846c8ebf8027f058c159b8f8a26b7cf060252e
MafeDiaz <125712070+MafeDiaz@users.noreply.github.com>
2023-09-15 21:44:52 -0500

    Add files via upload

---
+++ src/app/noticia-detalle/noticia-detalle.page.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { noticiasData } from '../data/noticias-data';
+import { Location } from '@angular/common'; 
+
+@Component({
+  selector: 'app-noticia-detalle',
+  templateUrl: './noticia-detalle.page.html',
+  styleUrls: ['./noticia-detalle.page.scss'],
+})
+export class NoticiaDetallePage implements OnInit {
+
+  noticia: any;
+
+  constructor(private route: ActivatedRoute, private location: Location) { }
+
+  ngOnInit() {
+    const noticiaIndexParam = this.route.snapshot.paramMap.get('id');
+    if (noticiaIndexParam !== null) {
+      const noticiaIndex = +noticiaIndexParam;
+      this.noticia = noticiasData[0].detailNoticias[noticiaIndex];
+    }
+  }
+
+  goBack() {
+    this.location.back();
+  }
+
+}
